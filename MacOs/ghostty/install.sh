5ba69bfa57a8afb4f2d653751e1aac59d12982a0
Jose Montana <jamontanac@unal.edu.co>
2025-05-05 16:19:10 -0500

    updating the install of ghostty

--- MacOs/ghostty/install.sh
+++ MacOs/ghostty/install.sh
@@ -1,8 +1,16 @@
 
 #use homebrew to install ghostty
 echo "Installing Ghostty..."
-brew install --cask ghostty
-echo "Ghostty installed successfully."
+# check if ghostty is already installed with cask or it is already in Applications
+if [ -d "/Applications/Ghostty.app" ]; then
+    echo "Ghostty is already installed in Applications."
+elif brew list --cask | grep -q ghostty; then
+    echo "Ghostty is already installed."
+else
+    brew install --cask ghostty
+    echo "Ghostty installed successfully."
+fi
+
 echo "Setting up Ghostty..."
 # Set up Ghostty
 # make sure the folder ~/.config/ghostty exists
