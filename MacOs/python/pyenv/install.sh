b398df91ab498c80ab6ebad0bd48c4b5dde727e0
Jose Montana <jamontanac@unal.edu.co>
2025-05-06 15:54:34 -0500

    updating the installer

--- MacOs/python/pyenv/install.sh
+++ MacOs/python/pyenv/install.sh
@@ -3,3 +3,5 @@ echo "Installing pyenv..."
 brew install pyenv pyenv-virtualenv
 echo "pyenv installed successfully."
 echo $(pyenv --version)
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+/bin/zsh -c "source ~/.zshrc"
