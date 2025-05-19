71925fc34d4e920a90b6bff068167c7d31a03687
Jose Montana <jamontanac@unal.edu.co>
2025-05-19 11:53:46 -0500

    adding the vimrc file

--- MacOs/vim/install.sh
+++ MacOs/vim/install.sh
@@ -11,6 +11,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # check with brew if each package is installed and install it if not
 
-
+# copy the vimrc file to ~/.vimrc
+cp vim/.vimrc ~/.vimrc
 # set ripgrep for fzf as default 
-/bin/zsh -c "source ~/.zshrc"
+/bin/zsh -c "source ~/.vimrc"
