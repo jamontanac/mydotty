346f4fea58545d1552b487468e9c6f866a52e0c2
Jose Montana <jamontanac@unal.edu.co>
2025-06-29 19:02:20 -0500

    adding new autoformating to use conform with mason to gurantee the existance of the formatters.

--- MacOs/nvim/lua/plugins/misc.lua
+++ MacOs/nvim/lua/plugins/misc.lua
@@ -1,14 +1,20 @@
 return {
-  { "tpope/vim-surround" },
-  { "tpope/vim-sleuth" },
-  { "tpope/vim-repeat" },
-  { "norcalli/nvim-colorizer.lua" },
-  {
-    "jiaoshijie/undotree",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = true,
-    keys = { -- load the plugin only when using it's keybinding:
-      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    { 'tpope/vim-surround' },
+    { 'tpope/vim-sleuth' },
+    { 'tpope/vim-repeat' },
+    { 'norcalli/nvim-colorizer.lua' },
+    {
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = { signs = false },
     },
-  },
+    {
+        'jiaoshijie/undotree',
+        dependencies = 'nvim-lua/plenary.nvim',
+        config = true,
+        keys = { -- load the plugin only when using it's keybinding:
+            { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
 }
