1719f0d962a9efc79ded80dd4617926992f85d0a
Jose Alejandro Montana <jamontanac@unal.edu.co>
2026-03-09 22:27:47 -0500

    adding the new rout for macos

--- MacOs/lazygit/install.sh
+++ MacOs/lazygit/install.sh
@@ -14,10 +14,8 @@ else
     echo "Git Delta installed successfully."
 fi
 #writing a yaml file with the config for the pager of delta
-mkdir -p .config/lazygit
 echo -e '
 git: 
     paging:
-        colorArg: always
-        pager: delta --dark --paging=never --syntax-theme base16-256 --diff-so-fancy (or -s for split)
-' >> .config/lazygit/config.yml
+        pager: delta --dark --paging=never --syntax-theme base16-256 --diff-so-fancy --side-by-side --line-numbers
+' >> ~/Library/Application/ Support/lazygit/config.yml
