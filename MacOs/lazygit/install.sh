06a0f8ac339da45c631fffb1faa2399b3f3dba7b
Jose Alejandro Montana <jamontanac@unal.edu.co>
2026-03-09 21:36:35 -0500

    adding the new way of setting the pager as delta

--- MacOs/lazygit/install.sh
+++ MacOs/lazygit/install.sh
@@ -6,3 +6,18 @@ else
     brew install lazygit
     echo "LazyGit installed successfully."
 fi
+if brew git-delta &>/dev/null; then
+    echo "Git Delta already installed"
+else
+    echo "Installing Git Delta..."
+    brew install git-delta
+    echo "Git Delta installed successfully."
+fi
+#writing a yaml file with the config for the pager of delta
+mkdir -p .config/lazygit
+echo -e '
+git: 
+    paging:
+        colorArg: always
+        pager: delta --dark --paging=never --syntax-theme base16-256 --diff-so-fancy (or -s for split)
+' >> .config/lazygit/config.yml
