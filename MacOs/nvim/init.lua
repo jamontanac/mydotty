de441f491fab0860576a9f587d48fb6be4c333c6
Jose Alejandro Montana <jamontanac@unal.edu.co>
2026-06-12 15:46:50 -0500

    Add mdmath plugin for enhanced markdown equation rendering

--- MacOs/nvim/init.lua
+++ MacOs/nvim/init.lua
@@ -34,6 +34,7 @@ require('lazy').setup {
     -- This particular plugins has problems when loading icons
     -- require 'plugins.new_bufferline',
     require 'plugins.markdown',
+    require 'plugins.mdmath',
     -- require 'plugins.nabla',
     require 'plugins.obsidian',
     require 'plugins.lualine',
