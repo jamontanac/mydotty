c874c0fe5ecb47b54a2c291637667d1a835535db
Jose Alejandro Montana <jamontanac@unal.edu.co>
2026-06-16 11:35:06 -0500

    Enable line wrapping and break indent for markdown file types

--- MacOs/nvim/lua/core/options.lua
+++ MacOs/nvim/lua/core/options.lua
@@ -51,6 +51,9 @@ vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'markdown', 'mdx', 'quarto' },
     callback = function()
         vim.opt_local.conceallevel = 2
+        vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
+        vim.opt_local.breakindent = true
     end,
 })
 
